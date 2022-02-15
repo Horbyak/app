52c70265a3c26d5c60ab23675a235a47fc4a6c7a
Enejenej <horbyakb@gmail.com>
2022-02-15 23:31:32 +0200

    Home work 1

---
+++ index.js
@@ -0,0 +1,116 @@
+/*Лекція № 1 - типи даних вивести в консоль*/
+
+/*let string = "string"
+
+console.log(typeof string);*///string
+
+
+/*let i;
+let a = null;
+i = 5
+
+console.log(i, a);*///5 null
+
+
+/*let number1 = 5 , number2 = "5";
+
+let sum = number1 + number2;
+
+console.log(sum);*///55
+
+
+/*let number1 = 5 , number2 = "5";
+
+let sum = number1 + +number2;
+
+console.log(sum);*///10
+
+
+/*let number1 = 5 , number2 = "5";
+
+let sum = number1 + number2;
+
+console.log(typeof navber1, typeof navber2);*///undefined undefined
+
+
+/*let myNumber = 125;
+
+console.log(typeof myNumber);*///number
+
+
+/*let myNumber = 125;
+
+console.log(typeof String (myNumber));
+
+console.log(String (myNumber));*///string 125
+
+
+/*let myNumber = 125;
+
+let newNumber = String(myNumber);
+
+console.log(newNumber);*/ //125
+
+
+/*let myNumber = "125";
+
+let newNumber = Number(myNumber);
+
+console.log(newNumber);*///125
+
+/*2. Вивчити всі типи даних та вивести всі їх в консоль
+  (вивести значення змінної і поряд з ним тип тоі змінної)*/
+  
+/*let myNumber = "125";
+
+let newNumber = typeof(125);
+
+console.log(myNumber,newNumber); *///125 //number 
+
+
+/*let mynumber = "125";
+
+alert(mynumber);*///125
+
+
+/*let Number = prompt("Введіть число");
+console.log(Number);*///ввів 125 є
+
+/*let Number1 = prompt("Введіть число");
+let Number2 = prompt("Введіть число", 5);
+let sum = Number1 + Number2;
+alert(sum);*///ввели 2 отр.: 25
+
+/*let Number1 = prompt("Введіть число");
+let Number2 = prompt("Введіть число", 11111);
+let sum = Number1 + Number2;
+alert(sum);*/
+
+
+/*4. Alert, prompt створити дві змінні та за допомогою prompt 
+ввести значення, знайти суму цих змінних та за допомогою alert,
+ вивести результат, після чого ввести ще
+ одне значення з клавіатури і його поділити на 2*/
+
+/*let Number1 = +prompt("Введіть число");
+let Number2 = +prompt("Введіть число");
+let sum = Number1 + Number2;
+alert(sum);
+let Number3 = prompt("Введіть число");
+let Number4 = 2
+let sum1 = Number3 / Number4;
+alert(sum1);*/ //є
+
+
+/*додає*/
+/*let Number1 = Number(prompt("Введіть число"));
+let Number2 = Number(prompt("Введіть число", 5));
+let sum = Number1 + Number2;
+alert(sum);*/
+
+/*let guestion = confirm("Ваш вік 20?");
+
+console.log(guestion);*///ok true, ckacyvatu folse
+
+
+  
